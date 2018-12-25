9d5ee27dab68063327834228ac5eac46b1d00baf
Aleksas <>
2018-12-25 11:49:47 +0800

    k fold score

--- KNN_model.py
+++ KNN_model.py
@@ -6,7 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import decomposition
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
+from math import sqrt
 import random
 
 
@@ -38,15 +40,32 @@ def run_knn(dataset, features, neighbors, appendable_features = [], enable_scale
         pca = decomposition.PCA(n_components=PCA)
         X = pca.fit_transform(X)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    info = []
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    kf = KFold(n_splits=10, shuffle=True)
+    for train, test in kf.split(X):
+        X_train = X[train]
+        X_test = X[test]
+        y_train = y[train]
+        y_test = y[test]
 
-    classifier = KNeighborsRegressor(n_neighbors=neighbors)
-    classifier.fit(X_train, y_train)
+        model = KNeighborsRegressor(n_neighbors=neighbors)
+        model.fit(X_train, y_train)
 
-    y_pred_train = classifier.predict(X_train)
-    y_pred = classifier.predict(X_test)
+        y_pred_train = model.predict(X_train)
+        y_pred = model.predict(X_test)
 
-    info = [mean_squared_error(y_test, y_pred), r2_score(y_test, y_pred), mean_squared_error(y_train, y_pred_train), r2_score(y_train, y_pred_train)]
-    return info
+        info_row = [mean_squared_error(y_test, y_pred), r2_score(y_test, y_pred), sqrt(mean_squared_error(y_test, y_pred)), mean_squared_error(y_train, y_pred_train), r2_score(y_train, y_pred_train), sqrt(mean_squared_error(y_train, y_pred_train))]
+        info.append(info_row)
 
-print(run_knn(dataset, [6, 11, 12], 10, [3], True))
+    stats = [0, 0, 0, 0, 0, 0]
+    for i in range(10):
+        for j in range(6):
+            stats[j] += info[i][j]
+
+    for j in range(6):
+        stats[j] /= 10
+    return stats
+
+stats = run_knn(dataset, [6, 11, 12], 10, [3], True)
+print(stats)
